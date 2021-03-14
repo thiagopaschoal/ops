9e518d640f42b395c022ea7bffbe80d53ccf09da
Thiago Sakurai Paschoal <thiagopaschoal076@gmail.com>
2021-03-14 16:53:40 -0300

    feat: add security group to allow ssh

--- main.tf
+++ main.tf
@@ -3,11 +3,26 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["201.69.194.202/32"]
+  }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
 resource "aws_instance" "ec2-dev" {
-  count         = 3
-  ami           = "ami-038f1ca1bd58a5790"
-  instance_type = "t2.micro"
-  key_name      = "terraform-aws"
+  count                  = 2
+  ami                    = "ami-038f1ca1bd58a5790"
+  instance_type          = "t2.micro"
+  key_name               = "terraform-aws"
+  vpc_security_group_ids = ["sg-080701986416931fa"]
   tags = {
     "Name" = "ec2-dev-${count.index}"
   }
